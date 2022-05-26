4873e9376a32e5a16cd24a34436a8730048135d4
raon.c <raon.c@kakaoent.com>
2022-05-26 16:08:41 +0900

    PR comment

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,7 +7,9 @@ module.exports = {
       numberOfRuns: 5, // Lighthouse 가 5번 실행됨
     },
     upload: {
-      target: "temporary-public-storage", // Lighthouse 서버에 결과가 업로드됨 명령어 실행 후 확인할 수 있는 link가 생성됨
+      target: "filesystem",
+      outputDir: "./lhci_reports",
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
     },
     assert: {
       assertions: {
