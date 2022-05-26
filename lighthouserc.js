121d451f8716936a825deb8525d25a373bf7f582
raon.c <raon.c@kakaoent.com>
2022-05-26 16:29:39 +0900

    job id 수정

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       staticDistDir: "./build", // 빌드 결과물의 위치
       // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
       url: ["http://localhost:3000"],
-      numberOfRuns: 5, // Lighthouse 가 5번 실행됨
+      numberOfRuns: 1, // Lighthouse 가 5번 실행됨
     },
     upload: {
       target: "filesystem",
